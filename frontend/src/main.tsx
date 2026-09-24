606fddc5a7e5d03ad3967b8ae3dff63bf1a6a647
ylx <a9634263@gmail.com>
2026-09-24 23:59:11 +0800

    save

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 //component->App(根主件)->main 
+
 import { App } from "@/App";
 import "@/styles/globals.css";
 
